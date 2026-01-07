34dac7b13a6bc9eea835360c2fd4735d8cad3a2d
fayisRahman <we.are.thadathil@gmail.com>
2026-01-07 11:02:06 +0000

    STAGE 16 COMPLETED

---
+++ stage16/src/config/xps_config.c
@@ -0,0 +1,403 @@
+#include "xps_config.h"
+
+void parse_server(JSON_Object *server_object, xps_config_server_t *server);
+void parse_listener(JSON_Object *listener_object,
+                    xps_config_listener_t *listener);
+void parse_route(JSON_Object *route_object, xps_config_route_t *route);
+void parse_all_listeners(vec_void_t *_all_listeners,
+                         xps_config_server_t *server);
+
+xps_config_t *xps_config_create(const char *config_path) {
+  /*assert*/
+  assert(config_path != NULL);
+  /*allocate mem for config*/
+  xps_config_t *config = malloc(sizeof(xps_config_t));
+  if (config == NULL) {
+    logger(LOG_ERROR, "xps_config_create()", "malloc() failed for config");
+    return NULL;
+  }
+  /*get config_json using json_parse_file*/
+  JSON_Value *config_json = json_parse_file(config_path);
+  if (config_json == NULL) {
+    logger(LOG_ERROR, "xps_config_create()", "json_parse_file() failed");
+    return NULL;
+  }
+  /*initialize fields of config object*/
+  config->_config_json = config_json;
+  config->config_path = config_path;
+
+  JSON_Object *root_object = json_value_get_object(config_json);
+  if (root_object == NULL) {
+    logger(LOG_ERROR, "xps_config_create()", "failed to parse root_object");
+    return NULL;
+  }
+  /*initialize server_name,workers,servers fields - hint: use
+  json_object_get_string ,json_object_get_number,json_object_get_array*/
+  config->server_name = json_object_get_string(root_object, "server_name");
+  config->workers = json_object_get_number(root_object, "workers");
+
+  /*Setting Up `server` Array*/
+  JSON_Array *servers = json_object_get_array(root_object, "servers");
+  vec_init(&(config->servers));
+  if (servers == NULL) {
+    logger(LOG_ERROR, "xps_config_create()", "failed to parse servers");
+    return NULL;
+  }
+  for (size_t i = 0; i < json_array_get_count(servers); i++) {
+    JSON_Object *server_object = json_array_get_object(servers, i);
+    if (server_object == NULL) {
+      logger(LOG_ERROR, "xps_config_create()", "failed to parse server_object");
+      return NULL;
+    }
+    xps_config_server_t *server = malloc(sizeof(xps_config_server_t));
+    if (server == NULL) {
+      logger(LOG_ERROR, "xps_config_create()", "malloc() failed for server");
+      return NULL;
+    }
+    vec_init(&(server->routes));
+    vec_init(&(server->listeners));
+    vec_init(&server->hostnames);
+    parse_server(server_object, server);
+    vec_push(&(config->servers), server);
+  }
+
+  /*Setting up `_all_listeners` Array*/
+  vec_init(&(config->_all_listeners));
+  for (int i = 0; i < config->servers.length; i++) {
+    parse_all_listeners(&(config->_all_listeners), config->servers.data[i]);
+  }
+
+  logger(LOG_DEBUG, "xps_config_create()", "Config file parsed successfully");
+  return config;
+}
+
+void xps_config_destroy(xps_config_t *config) {
+  assert(config != NULL);
+
+  for (int i = 0; i < config->servers.length; i++) {
+    xps_config_server_t *server = config->servers.data[i];
+    for (int j = 0; j < server->listeners.length; j++)
+      free(server->listeners.data[j]);
+    vec_deinit(&server->listeners);
+    for (int j = 0; j < server->routes.length; j++) {
+      xps_config_route_t *route = server->routes.data[j];
+      vec_deinit(&(route->index));
+      vec_deinit(&(route->upstreams));
+      free(route);
+    }
+    vec_deinit(&(server->routes));
+    vec_deinit(&(server->hostnames));
+    free(server);
+  }
+  vec_deinit(&(config->servers));
+
+  json_value_free(config->_config_json);
+  free(config);
+}
+
+xps_config_lookup_t *xps_config_lookup(xps_config_t *config,
+                                       xps_http_req_t *http_req,
+                                       xps_connection_t *client, int *error) {
+  /*assert*/
+  assert(config != NULL);
+  assert(http_req != NULL);
+  assert(client != NULL);
+
+  *error = E_FAIL;
+  /*get host,keep_alive(connection),accept encoding,pathname from http_req*/
+
+  const char *h_host = xps_http_get_header(&(http_req->headers), "Host");
+  const char *h_keep_alive =
+      xps_http_get_header(&(http_req->headers), "Connection");
+  const char *h_accept_encoding =
+      xps_http_get_header(&(http_req->headers), "Accept-Encoding");
+  const char *h_pathname = http_req->pathname;
+  // Step 1: Find matching server block
+  int target_server_index = -1;
+  for (int i = 0; i < config->servers.length; i++) {
+    xps_config_server_t *server = config->servers.data[i];
+    // Check if client listener is present in server
+    bool has_matching_listener = false;
+    for (int j = 0; j < server->listeners.length; j++) {
+      xps_config_listener_t *listener = server->listeners.data[j];
+      if (strcmp(listener->host, client->listener->host) == 0 &&
+          listener->port == client->listener->port) {
+        has_matching_listener = true;
+        break;
+      }
+    }
+    if (!has_matching_listener)
+      continue;
+
+    /* Check if host header matches any hostname*/
+    // NOTE: if not hostnames provided, it is considered a match
+    bool has_matching_hostname = server->hostnames.length == 0;
+    for (int j = 0; j < server->hostnames.length; j++) {
+      if (strcmp(server->hostnames.data[j], h_host) == 0) {
+        has_matching_hostname = true;
+        break;
+      }
+    }
+    if (has_matching_hostname) {
+      target_server_index = i;
+      break;
+    }
+  }
+
+  if (target_server_index == -1) {
+    *error = E_NOTFOUND;
+    return NULL;
+  }
+
+  xps_config_server_t *server = config->servers.data[target_server_index];
+  /*Find matching route block*/
+  xps_config_route_t *route = NULL;
+  int longest_path_len = -1;
+  for (int i = 0; i < server->routes.length; i++) {
+    xps_config_route_t *current_route = server->routes.data[i];
+    size_t route_path_len = strlen(current_route->req_path);
+
+    if (str_starts_with(h_pathname, current_route->req_path)) {
+
+      if ((int)route_path_len > longest_path_len) {
+        route = current_route;
+        longest_path_len = (int)route_path_len;
+      }
+    }
+  }
+
+  if (route == NULL) {
+    *error = E_NOTFOUND; // No matching route found - 404
+    return NULL;
+  }
+
+  /* Init values of lookup*/
+  xps_config_lookup_t *lookup = malloc(sizeof(xps_config_lookup_t));
+  if (lookup == NULL) {
+    logger(LOG_ERROR, "xps_config_lookup()", "malloc() failed for 'lookup'");
+    return NULL;
+  }
+  if (strcmp(route->type, "file_serve") == 0) {
+    lookup->type = REQ_FILE_SERVE;
+
+  } else if (strcmp(route->type, "reverse_proxy") == 0)
+    lookup->type = REQ_REVERSE_PROXY;
+  else if (strcmp(route->type, "redirect") == 0)
+    lookup->type = REQ_REDIRECT;
+  else
+    lookup->type = REQ_INVALID;
+  // Initialize common fields
+  lookup->keep_alive = h_keep_alive && strcmp(h_keep_alive, "keep-alive") == 0;
+  /*need to understand why these values*/
+  lookup->file_path = NULL;
+  lookup->dir_path = NULL;
+  lookup->file_start = 0;
+  lookup->file_end = -1;
+  lookup->upstream =
+      route->upstreams.length > 0 ? route->upstreams.data[0] : NULL;
+  /*till here |^*/
+  lookup->http_status_code = route->http_status_code;
+  lookup->redirect_url = route->redirect_url;
+
+  // Initialize type-specific fields (need to specify in doc what to do here cuz
+  // TODO: its too vague on what we are doing here)
+  if (lookup->type == REQ_FILE_SERVE) {
+    char *resource_path = path_join(
+        route->dir_path,
+        h_pathname +
+            strlen(route->req_path)); /*need to specify this in the doc*/
+    if (!is_abs_path(
+            resource_path)) { // we require abosulte path so we need to see if
+                              // the current path is absolute or not
+      char *temp = str_create(config->config_path);
+      char *temp2 = path_join(dirname(temp), resource_path);
+      free(temp);
+      free(resource_path);
+      resource_path = temp2;
+    }
+    // is file
+    if (is_file(resource_path)) {
+      lookup->file_path = resource_path;
+
+    } else if (is_dir(resource_path)) { // is directory
+      bool index_file_found = false;
+      for (int i = 0; i < route->index.length; i++) {
+        char *index_file = path_join(resource_path, route->index.data[i]);
+        if (is_file(index_file)) {
+          lookup->file_path = index_file;
+          index_file_found = true;
+          free(resource_path);
+          break;
+        } else {
+          free(index_file);
+        }
+      }
+      if (!index_file_found) {
+        // TDOD: check this out and need to see if it is there or not
+        free(resource_path);
+        // Leave lookup->file_path as NULL (already set on line 202)
+      }
+    } else {
+      free(resource_path);
+    }
+    logger(LOG_DEBUG, "xps_config_file_lookup()", "requested file path: %s",
+           lookup->file_path);
+    *error = OK;
+  }
+  *error = OK;
+  return lookup;
+}
+
+void xps_config_lookup_destroy(xps_config_lookup_t *config_lookup,
+                               xps_core_t *core) {
+  assert(config_lookup != NULL);
+  assert(core != NULL);
+
+  if (config_lookup->dir_path)
+    free(config_lookup->dir_path);
+
+  if (config_lookup->file_path)
+    free(config_lookup->file_path);
+
+  free(config_lookup);
+}
+
+void parse_server(JSON_Object *server_object, xps_config_server_t *server) {
+
+  /*Setting Up `listeners` Array*/
+  JSON_Array *listeners = json_object_get_array(server_object, "listeners");
+
+  if (listeners)
+    for (size_t j = 0; j < json_array_get_count(listeners); j++) {
+      JSON_Object *listener_object = json_array_get_object(listeners, j);
+      xps_config_listener_t *listener = malloc(sizeof(xps_config_listener_t));
+      if (listener == NULL) {
+        logger(LOG_ERROR, "parse_server()", "malloc() failed for listener");
+        return;
+      }
+      parse_listener(listener_object, listener);
+      vec_push(&(server->listeners), listener);
+    }
+
+  /*Setting up `routes` Array*/
+  JSON_Array *routes = json_object_get_array(server_object, "routes");
+  if (routes)
+    for (size_t j = 0; j < json_array_get_count(routes); j++) {
+      JSON_Object *route_object = json_array_get_object(routes, j);
+      xps_config_route_t *route = malloc(sizeof(xps_config_route_t));
+      if (route == NULL) {
+        logger(LOG_ERROR, "parse_server()", "malloc() failed for route");
+        return;
+      }
+      vec_init(&(route->upstreams));
+      vec_init(&(route->index));
+      parse_route(route_object, route);
+      vec_push(&(server->routes), route);
+    }
+
+  /*Setting up `hostnames` Array*/
+  JSON_Array *hostnames = json_object_get_array(server_object, "hostnames");
+  if (hostnames)
+    for (int i = 0; i < json_array_get_count(hostnames); i++) {
+      const char *hostname = json_array_get_string(hostnames, i);
+      vec_push(&server->hostnames, (void *)hostname);
+    }
+}
+
+void parse_listener(JSON_Object *listener_object,
+                    xps_config_listener_t *listener) {
+  listener->host = json_object_get_string(listener_object, "host");
+  if (listener->host == NULL) {
+    logger(LOG_ERROR, "parse_listener()", "host is required");
+    return;
+  }
+  listener->port = json_object_get_number(listener_object, "port");
+  if (listener->port == 0) {
+    logger(LOG_ERROR, "parse_listener()", "port is required");
+    return;
+  }
+}
+
+void parse_route(JSON_Object *route_object, xps_config_route_t *route) {
+
+  route->req_path = json_object_get_string(route_object, "req_path");
+
+  if (!route->req_path) {
+    logger(LOG_ERROR, "parse_route()", "failed to parse req_path");
+    return;
+  }
+
+  route->type = json_object_get_string(route_object, "type");
+  if (strcmp(route->type, "file_serve") &&
+      strcmp(route->type, "reverse_proxy") && strcmp(route->type, "redirect")) {
+    logger(LOG_ERROR, "parse_route()", "invalid route type");
+    return;
+  }
+
+  if (strcmp(route->type, "file_serve") == 0) {
+
+    /*if file server */
+    route->dir_path = json_object_get_string(route_object, "dir_path");
+    if (route->dir_path == NULL) {
+      logger(LOG_ERROR, "parse_route()", "dir_path is required");
+      return;
+    }
+    JSON_Array *indexes = json_object_get_array(route_object, "index");
+    if (indexes)
+      for (size_t k = 0; k < json_array_get_count(indexes); k++) {
+        const char *index = json_array_get_string(indexes, k);
+        vec_push(&(route->index), (void *)index);
+      }
+
+  } else if (strcmp(route->type, "redirect") == 0) {
+
+    /*if redirect*/
+    route->http_status_code =
+        json_object_get_number(route_object, "http_status_code");
+    route->redirect_url = json_object_get_string(route_object, "redirect_url");
+    if (route->redirect_url == NULL) {
+      logger(LOG_ERROR, "parse_route()", "redirect_url required");
+      return;
+    }
+
+  } else if (strcmp(route->type, "reverse_proxy") == 0) {
+
+    /*if upstream*/
+    JSON_Array *upstreams = json_object_get_array(route_object, "upstreams");
+
+    if (upstreams == NULL) {
+      logger(LOG_ERROR, "parse_route()", "upstreams is required");
+      return;
+    }
+    for (size_t k = 0; k < json_array_get_count(upstreams); k++) {
+      const char *upstream = json_array_get_string(upstreams, k);
+      vec_push(&(route->upstreams), (void *)upstream);
+    }
+  }
+}
+
+void parse_all_listeners(vec_void_t *_all_listeners,
+                         xps_config_server_t *server) {
+
+  for (int i = 0; i < server->listeners.length; i++) {
+
+    xps_config_listener_t *server_listener = server->listeners.data[i];
+
+    /*check if this listener is in the _all_listener array*/
+    bool listener_found = false;
+
+    for (int j = 0; j < _all_listeners->length; j++) {
+      xps_config_listener_t *curr = _all_listeners->data[i];
+
+      if (strcmp(server_listener->host, curr->host) == 0 &&
+          server_listener->port == curr->port) {
+        listener_found = true;
+        break;
+      }
+    }
+
+    if (!listener_found) {
+      vec_push(_all_listeners, server_listener);
+    }
+  }
+}
---
+++ stage16/src/network/xps_listener.c
@@ -0,0 +1,160 @@
+#include "xps_listener.h"
+
+xps_listener_t *xps_listener_create(const char *host, u_int port) {
+  assert(host != NULL);
+  assert(is_valid_port(port)); // Will be explained later
+
+  // Create socket instance
+  int sock_fd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+  if (sock_fd < 0) {
+    logger(LOG_ERROR, "xps_listener_create()", "socket() failed");
+    perror("Error message");
+    return NULL;
+  }
+
+  // Make address reusable
+  const int enable = 1;
+  /* make socket address reusable using setsockopt() */
+  if (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0) {
+    logger(LOG_ERROR, "xps_listener_create()", "setsockopt() failed");
+    perror("Error message");
+    close(sock_fd);
+    return NULL;
+  }
+
+  // Setup listener address
+  struct addrinfo *addr_info =
+      xps_getaddrinfo(host, port); // Will be explained later
+  if (addr_info == NULL) {
+    logger(LOG_ERROR, "xps_listener_create()", "xps_getaddrinfo() failed");
+    close(sock_fd);
+    return NULL;
+  }
+
+  // Binding to port
+  if (bind(sock_fd, addr_info->ai_addr, addr_info->ai_addrlen) < 0) {
+    logger(LOG_ERROR, "xps_listener_create()", "failed to bind() to %s:%u",
+           host, port);
+    perror("Error message");
+    freeaddrinfo(addr_info); // Will be explained later
+    close(sock_fd);
+    return NULL;
+  }
+  freeaddrinfo(addr_info); // Will be explained later
+
+  // Listening on port
+  if (listen(sock_fd, DEFAULT_BACKLOG) < 0) {
+    logger(LOG_ERROR, "xps_listener_create()", "listen() failed");
+    perror("Error message");
+    close(sock_fd);
+    return NULL;
+  }
+
+  // Create & allocate memory for a listener instance
+  xps_listener_t *listener = malloc(sizeof(xps_listener_t));
+  if (listener == NULL) {
+    logger(LOG_ERROR, "xps_listener_create()",
+           "malloc() failed for 'listener'");
+    close(sock_fd);
+    return NULL;
+  }
+
+  // Init values
+  listener->core = NULL; // Initialize to NULL since not attached to core yet
+  listener->host = host;
+  listener->port = port;
+  listener->sock_fd = sock_fd;
+
+  // // Attach listener to loop
+  // xps_loop_attach(core->loop, sock_fd, EPOLLIN | EPOLLET, listener,
+  //                 xps_listener_connection_handler, NULL, NULL);
+
+  // // Add listener to 'listeners' list
+  // vec_push(&(core->listeners), listener);
+
+  logger(LOG_DEBUG, "xps_listener_create()", "created listener on port %d",
+         port);
+
+  return listener;
+}
+
+void xps_listener_destroy(xps_listener_t *listener) {
+
+  // Validate params
+  assert(listener != NULL);
+
+  // TODO: SPECIFY TO ADD THSI CONDITION AS WELL
+
+  if (listener->core != NULL) {
+    // Detach listener from loop
+    xps_loop_detach(listener->core->loop, listener->sock_fd);
+
+    // Set listener to NULL in 'listeners' list
+    for (int i = 0; i < (listener->core)->listeners.length; i++) {
+      xps_listener_t *curr = (listener->core)->listeners.data[i];
+      if (curr == listener) {
+        (listener->core)->listeners.data[i] = NULL;
+        break;
+      }
+    }
+  }
+
+  // Close socket
+  close(listener->sock_fd);
+
+  logger(LOG_DEBUG, "xps_listener_destroy()", "destroyed listener on port %d",
+         listener->port);
+
+  // Free listener instance
+  free(listener);
+}
+
+void xps_listener_connection_handler(void *ptr) {
+  assert(ptr != NULL);
+  xps_listener_t *listener = ptr;
+
+  while (1) {
+    struct sockaddr conn_addr;
+    socklen_t conn_addr_len = sizeof(conn_addr);
+
+    // Accepting connection
+    int conn_sock_fd = accept(listener->sock_fd, &conn_addr, &conn_addr_len);
+
+    if (conn_sock_fd < 0 && (errno == EAGAIN || errno == EWOULDBLOCK))
+      break;
+
+    if (conn_sock_fd < 0) {
+      logger(LOG_ERROR, "xps_listener_connection_handler()", "accept() failed");
+      perror("Error message");
+      return;
+    }
+
+    if (make_socket_non_blocking(conn_sock_fd) != OK) {
+      logger(LOG_ERROR, "xps_listener_create()",
+             "make_socket_non_blocking() failed");
+      close(conn_sock_fd);
+      return;
+    }
+
+    // Creating connection instance
+    xps_connection_t *client = xps_connection_create(
+        listener->core, conn_sock_fd); // Will be implemented later
+    if (client == NULL) {
+      logger(LOG_ERROR, "xps_listener_connection_handler()",
+             "xps_connection_create() failed");
+      close(conn_sock_fd);
+      return;
+    }
+    client->listener = listener;
+
+    xps_session_t *session = xps_session_create(listener->core, client);
+    if (session == NULL) {
+      logger(LOG_ERROR, "listener_connection_handler()",
+             "xps_session_create() failed");
+      xps_connection_destroy(client);
+      return;
+    }
+
+    logger(LOG_INFO, "xps_listener_connection_handler()", "new connection");
+  }
+}
