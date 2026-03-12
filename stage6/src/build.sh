099cd15e7d6b1db680137e81aa5239841be66afe
fayisRahman <we.are.thadathil@gmail.com>
2026-03-12 03:03:22 +0000

    feat: added stage 6,7 and 8

---
+++ stage6/src/build.sh
@@ -0,0 +1,1 @@
+gcc -g -o xps main.c lib/vec/src/vec.c network/xps_connection.c network/xps_listener.c utils/xps_logger.c utils/xps_utils.c
---
+++ stage8/src/network/xps_connection.c
@@ -0,0 +1,171 @@
+#include "xps_connection.h"
+
+// Function declaration for read callback of listener
+void connection_loop_read_handler(void *ptr);
+void connection_loop_write_handler(void *ptr);
+void connection_loop_close_handler(void *ptr);
+
+xps_connection_t *xps_connection_create(xps_core_t *core, u_int sock_fd, xps_listener_t *listener) {
+
+    assert(core != NULL);
+    assert(listener != NULL);
+
+    xps_connection_t *connection = malloc(sizeof(xps_connection_t));
+    if (connection == NULL) {
+      logger(LOG_ERROR, "xps_connection_create()", "malloc() failed for 'connection'");
+      return NULL;
+    }
+
+    xps_buffer_list_t *write_buff_list = xps_buffer_list_create();
+    if (write_buff_list == NULL) {
+      logger(LOG_ERROR, "xps_connection_create()", "xps_buffer_list_create() failed");
+      free(connection);
+      return NULL;
+    }
+  
+    /* attach sock_fd to epoll */
+    xps_loop_attach(core->loop, sock_fd, EPOLLIN | EPOLLOUT, connection, connection_loop_read_handler, connection_loop_write_handler, connection_loop_close_handler);
+  
+    // Init values
+    connection->core = core;
+    connection->sock_fd = sock_fd;
+    connection->listener = NULL;
+    connection->remote_ip = get_remote_ip(sock_fd);
+    connection->write_buff_list = write_buff_list;
+  
+    /* add connection to 'connections' list */
+    vec_push(&(core->connections),connection);
+  
+    logger(LOG_DEBUG, "xps_connection_create()", "created connection");
+    return connection;
+  
+  }
+  
+void xps_connection_destroy(xps_connection_t *connection) {
+  
+    /* validate params */
+    assert(connection != NULL);
+  
+    /* set connection to NULL in 'connections' list */
+    for (int i = 0; i < (connection->core->connections).length; i++) {
+        xps_connection_t *curr = (connection->core->connections).data[i];
+        if (curr == connection) {
+          (connection->core->connections).data[i] = NULL;
+          break;
+        }
+      }
+    
+  
+    /* detach connection from loop */
+    xps_loop_detach(connection->core->loop,connection->sock_fd);
+  
+    /* close connection socket FD */
+    close(connection->sock_fd);
+  
+    /* free connection->remote_ip */
+    free(connection->remote_ip);
+  
+    /* free connection instance */
+    free(connection);
+  
+    logger(LOG_DEBUG, "xps_connection_destroy()", "destroyed connection");
+  
+}
+
+// Function to reverse a string in-place
+void connection_strrev(char *str) {
+    for (int start = 0, end = strlen(str) - 2; start < end; start++, end--) {
+      char temp = str[start];
+      str[start] = str[end];
+      str[end] = temp;
+    }
+}
+
+
+void connection_loop_read_handler(void* ptr) {
+
+    /* validate params */
+    assert(ptr != NULL);
+
+    xps_connection_t *connection = ptr;
+    
+    u_char buff[DEFAULT_BUFFER_SIZE];
+    memset(buff,0,sizeof(buff));
+
+    /* read data from client using recv() */
+    long read_n = recv(connection->sock_fd,buff,sizeof(buff),0);
+
+    if (read_n < 0) {
+      logger(LOG_ERROR, "connection_loop_read_handler()", "recv() failed");
+      perror("Error message");
+      xps_connection_destroy(connection);
+      return;
+    }
+  
+    if (read_n == 0) {
+      logger(LOG_INFO, "connection_loop_read_handler()", "peer closed connection");
+      xps_connection_destroy(connection);
+      return;
+    }
+  
+    buff[read_n] = '\0';
+  
+    /* print client message */
+    // printf("[CLIENT MESSAGE] %s", buff);
+  
+    /* reverse client message */
+    connection_strrev(buff);
+
+    xps_buffer_t* temp_buff = xps_buffer_create(read_n,read_n,NULL);
+
+    memcpy(temp_buff->data, buff, read_n);
+
+    xps_buffer_list_append(connection->write_buff_list, temp_buff);
+  
+  
+}
+
+void connection_loop_write_handler(void* ptr){
+
+  assert(ptr != NULL);
+
+  xps_connection_t* connection = ptr;
+
+  if (connection->write_buff_list->len == 0)
+    return;
+
+  // Sending reversed message to client
+  long bytes_written = 0;
+  long message_len = connection->write_buff_list->len;
+
+  xps_buffer_t* buff = xps_buffer_list_read(connection->write_buff_list,connection->write_buff_list->len);
+
+  long write_n = send(connection->sock_fd,buff->data, buff->len, 0);
+
+  if (write_n == -1 && !(errno == EAGAIN || errno == EWOULDBLOCK)) {
+    logger(LOG_ERROR, "connection_loop_write_handler()", "send() failed");
+    perror("Error message");
+    xps_connection_destroy(connection);
+    return;
+  }
+
+  if (write_n > 0) {
+    xps_buffer_list_clear(connection->write_buff_list, write_n);
+    return;
+  }
+   
+
+}
+
+void connection_loop_close_handler(void* ptr){
+
+  assert(ptr != NULL);
+
+  xps_connection_t* connection = ptr;
+
+  xps_connection_destroy(connection);
+
+  logger(LOG_INFO, "connection_loop_close_handler()", "peer closed connection");
+
+}
+
